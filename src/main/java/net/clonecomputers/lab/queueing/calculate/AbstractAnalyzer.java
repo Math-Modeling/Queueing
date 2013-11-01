92ce742305dfd926737d493cc87df4533e041cd1
1000000000 <lthyde@gmail.com>
2013-11-01 00:32:40 -0400

    Added convenience method to AbstractAnalyzer to help write CSV to file

--- src/main/java/net/clonecomputers/lab/queueing/calculate/AbstractAnalyzer.java
+++ src/main/java/net/clonecomputers/lab/queueing/calculate/AbstractAnalyzer.java
@@ -1,17 +1,24 @@
 package net.clonecomputers.lab.queueing.calculate;
 
 import java.awt.Component;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 @SuppressWarnings("serial")
 public abstract class AbstractAnalyzer extends JPanel {
 	
 	private StatsMain main;
 	private List<InputField<?>> inputFields = new ArrayList<InputField<?>>();
+	private final JFileChooser fileChooser = new JFileChooser();
 	
 	public AbstractAnalyzer() {}
 	
@@ -34,6 +41,15 @@ public abstract class AbstractAnalyzer extends JPanel {
 			inputted.add(i.getInputted());
 		}
 		return inputted;
+	}
+	
+	public CSVPrinter getCsvPrinter() throws IOException {
+		int value = fileChooser.showSaveDialog(this.getParent());
+		if(value == JFileChooser.APPROVE_OPTION) {
+			return new CSVPrinter(new BufferedWriter(new FileWriter(fileChooser.getSelectedFile())), CSVFormat.EXCEL);
+		} else {
+			return null;
+		}
 	}
 	
 	@Override
