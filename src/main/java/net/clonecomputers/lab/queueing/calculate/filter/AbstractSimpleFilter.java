88e8b6ac067b414d0aea4b14c24ea155c723cd57
g_rocket <g.rocket1@gmail.com>
2013-11-04 20:53:25 -0500

    Made AbstractSimpleFilter utilize multiple events per DataSnapshot the DataSnapshot returned now contains a list of all the events that happened since the last time a DataSnapshot was returned

--- src/main/java/net/clonecomputers/lab/queueing/calculate/filter/AbstractSimpleFilter.java
+++ src/main/java/net/clonecomputers/lab/queueing/calculate/filter/AbstractSimpleFilter.java
@@ -8,14 +8,18 @@ public abstract class AbstractSimpleFilter extends AbstractFilter {
 	public abstract boolean acceptEvent(DataSnapshot e);
 	
 	double timeInProgress = 0;
+	List<DataSnapshot.QueueingEvent> eventsInProgress = new ArrayList<DataSnapshot.QueueingEvent>();
 	
 	public List<DataSnapshot> processEvent(DataSnapshot e) {
+		timeInProgress += e.getTime();
+		eventsInProgress.add(e.getEvent());
 		if(acceptEvent(e)) {
+			List<DataSnapshot> ret =  Collections.singletonList(new DataSnapshot(timeInProgress + e.getTime(),
+					e.getCustomersShopping(), e.getQueueLength(), e.getCashiersBusy(), eventsInProgress));
 			timeInProgress = 0;
-			return Collections.singletonList(new DataSnapshot(timeInProgress + e.getTime(), e.getCustomersShopping(),
-					e.getQueueLength(), e.getCashiersBusy(), (DataSnapshot.QueueingEvent)null));
+			eventsInProgress.clear();
+			return ret;
 		} else {
-			timeInProgress += e.getTime();
 			return null;
 		}
 	}
