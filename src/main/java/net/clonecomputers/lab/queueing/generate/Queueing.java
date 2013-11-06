461e2055c2b4ab5a5009a0063d07b0042f536ce6
g_rocket <g.rocket1@gmail.com>
2013-11-06 12:02:11 -0500

    Made Queueing print status updates every second on progress

--- src/main/java/net/clonecomputers/lab/queueing/generate/Queueing.java
+++ src/main/java/net/clonecomputers/lab/queueing/generate/Queueing.java
@@ -93,14 +93,19 @@ public class Queueing {
 
 	public void run() throws IOException {
 		System.out.println("Generating data");
-		for(long i = 0; i < maxIterations; i++){ // now discards last data, so must do one more iteration
-			// in order to get the desired number of iterations
+		long startTime = System.currentTimeMillis();
+		long lastReportTime = startTime;
+		for(long i = 0; i < maxIterations; i++){
+			if(System.currentTimeMillis() > lastReportTime + 1000){
+				System.out.printf("%.02f%%\n",100*((double)i)/maxIterations);
+				lastReportTime = System.currentTimeMillis();
+			}
 			double intervalLength = howLongCurrentStateWillLast();
 			csv.record(howLongCurrentStateWillLast());
 			updateTime(intervalLength);
 			updateState();
 		}
-		System.out.println("Done");
+		System.out.printf("Completed in %s\n");
 	}
 
 	private void updateState() {
