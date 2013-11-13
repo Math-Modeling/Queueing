f1a2c36c5f289635b8932a683a39e25708193aaa
g_rocket <g.rocket1@gmail.com>
2013-11-13 05:23:25 -0500

    fixed logic problem with which time you're talking about also removed the need for "skip last event"
    previously, the time for an event was wrongly counted as the time until
    that event took place.  it now is the time until another interesting
    event takes place.

--- src/main/java/net/clonecomputers/lab/queueing/calculate/filters/AbstractSimpleFilter.java
+++ src/main/java/net/clonecomputers/lab/queueing/calculate/filters/AbstractSimpleFilter.java
@@ -7,26 +7,25 @@ import net.clonecomputers.lab.queueing.calculate.*;
 public abstract class AbstractSimpleFilter extends AbstractFilter {
 	public abstract boolean acceptEvent(DataSnapshot e);
 	
-	protected boolean discardsLastEvent = true; // set to false in constructor for subclass if you don't want this
-	
-	List<DataSnapshot> buffer = null;
-	
 	double timeInProgress = 0;
 	List<DataSnapshot.QueueingEvent> eventsInProgress = new ArrayList<DataSnapshot.QueueingEvent>();
+	DataSnapshot snapshotInProgress = null;
 	
 	public List<DataSnapshot> processEvent(DataSnapshot e) {
 		List<DataSnapshot> ret = null;
-		if(discardsLastEvent) ret = buffer;
-		timeInProgress += e.getTime();
-		eventsInProgress.add(e.getEvent());
 		if(acceptEvent(e)) {
-			buffer =  Collections.singletonList(new DataSnapshot(timeInProgress,
-					e.getCustomersShopping(), e.getQueueLength(), e.getCashiersBusy(), eventsInProgress));
-			timeInProgress = 0;
+			if(snapshotInProgress != null) ret =  Collections.singletonList(new DataSnapshot(
+					timeInProgress, snapshotInProgress.getCustomersShopping(),
+					snapshotInProgress.getQueueLength(),
+					snapshotInProgress.getCashiersBusy(), eventsInProgress));
+			timeInProgress = e.getTime();
 			eventsInProgress.clear();
-			if(discardsLastEvent) return ret;
-			else return buffer;
+			eventsInProgress.addAll(e.getEvents());
+			snapshotInProgress = e;
+			return ret;
 		} else {
+			timeInProgress += e.getTime();
+			eventsInProgress.addAll(e.getEvents());
 			return null;
 		}
 	}
