369d84ef61d7d95db88508f2f0269012bf35bc88
g_rocket <g.rocket1@gmail.com>
2013-11-12 22:48:14 -0500

    add optional feature to discard last event on by defalut

--- src/main/java/net/clonecomputers/lab/queueing/calculate/filters/AbstractSimpleFilter.java
+++ src/main/java/net/clonecomputers/lab/queueing/calculate/filters/AbstractSimpleFilter.java
@@ -7,18 +7,25 @@ import net.clonecomputers.lab.queueing.calculate.*;
 public abstract class AbstractSimpleFilter extends AbstractFilter {
 	public abstract boolean acceptEvent(DataSnapshot e);
 	
+	protected boolean discardsLastEvent = true; // set to false in constructor for subclass if you don't want this
+	
+	List<DataSnapshot> buffer = null;
+	
 	double timeInProgress = 0;
 	List<DataSnapshot.QueueingEvent> eventsInProgress = new ArrayList<DataSnapshot.QueueingEvent>();
 	
 	public List<DataSnapshot> processEvent(DataSnapshot e) {
+		List<DataSnapshot> ret = null;
+		if(discardsLastEvent) ret = buffer;
 		timeInProgress += e.getTime();
 		eventsInProgress.add(e.getEvent());
 		if(acceptEvent(e)) {
-			List<DataSnapshot> ret =  Collections.singletonList(new DataSnapshot(timeInProgress + e.getTime(),
+			buffer =  Collections.singletonList(new DataSnapshot(timeInProgress + e.getTime(),
 					e.getCustomersShopping(), e.getQueueLength(), e.getCashiersBusy(), eventsInProgress));
 			timeInProgress = 0;
 			eventsInProgress.clear();
-			return ret;
+			if(discardsLastEvent) return ret;
+			else return buffer;
 		} else {
 			return null;
 		}
