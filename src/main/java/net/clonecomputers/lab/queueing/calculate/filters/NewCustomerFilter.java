dff8de0b520e60a04362c48ea5eacf1f52949778
g_rocket <g.rocket1@gmail.com>
2013-11-12 22:47:27 -0500

    remove irrelevant //TODO

--- src/main/java/net/clonecomputers/lab/queueing/calculate/filters/NewCustomerFilter.java
+++ src/main/java/net/clonecomputers/lab/queueing/calculate/filters/NewCustomerFilter.java
@@ -6,7 +6,6 @@ public class NewCustomerFilter extends AbstractSimpleFilter {
 
 	@Override
 	public boolean acceptEvent(DataSnapshot e) {
-		// TODO Auto-generated method stub
 		return e.getEvent() == DataSnapshot.QueueingEvent.SUPERMARKET_ARRIVE;
 	}
 
